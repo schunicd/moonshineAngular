d71ec20fba9daedf0b6ca79437779105df0bf9aa
schunicd <schunicd@sheridancollege.ca>
2021-06-17 20:01:04 -0400

    Fixing CSS

---
+++ ClientApp/.old_src/main.ts
@@ -0,0 +1,22 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment, firebaseConfig } from './environments/environment';
+import firebase from "firebase/app" //importing main functionality
+
+export function getBaseUrl() {
+  return document.getElementsByTagName('base')[0].href;
+}
+
+const providers = [
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+];
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+  firebase.initializeApp(firebaseConfig);
