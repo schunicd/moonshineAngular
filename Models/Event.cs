0d90c90cfff2808ff1fcae89375de1ce6a8dff03
muslmoha <muslmoha@sheridancolledge.ca>
2021-09-09 11:28:36 -0400

    Adjusted event object and API to work with new outline, so including band information so it can be used in the spotlight directly from the event DB, fixed test data

--- Models/Event.cs
+++ Models/Event.cs
@@ -10,17 +10,16 @@ namespace TheMoonshineCafe.Models
     {
         
         public int id {get; set; }
-        public string title { get; set; }
         public DateTime eventStart { get; set; }
         public DateTime eventEnd { get; set; }
         public DateTime refundCutOffDate { get; set; }
-        public int bandId { get; set; }
+        public string bandName { get; set; }
+        public string bandImagePath { get; set; }
+        public string bandLink { get; set; }
         public int maxNumberOfSeats { get; set; }
         public int currentNumberOfSeats { get; set; }
         public double ticketPrice { get; set; }
         public string description { get; set; }
-        public string googleCalID { get; set; }
-        public Band band { get; set; }
-        
+        public string googleCalID { get; set; }  
     }
 }
