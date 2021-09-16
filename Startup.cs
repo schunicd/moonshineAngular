097598e8e0cbb7c4185ba82d5eb0a6407f18ca95
schunicd <schunicd@sheridancollege.ca>
2021-09-16 15:47:59 -0400

    TestingDatabaseConnection onServer

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace TheMoonshineCafe
 
 
             services.AddDbContext<MoonshineCafeContext>(options => 
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
